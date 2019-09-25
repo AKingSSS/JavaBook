83ebd8edd7bf98ec4322ef7f058a8c5c13870925
kang.yang <Kang.Yang@b-and-qchina.com>
2019-09-25 10:39:47 +0800

    分享Java读书心得

---
+++ src/main/java/www/reader/com/Redis/id01chapters/R01.java
@@ -0,0 +1,11 @@
+package www.aking.com.EffectiveJava.id01chapters;
+
+/**
+ * @ClassName R01
+ * @Description TODO
+ * @Author yangkang
+ * @Date 2019/9/25 10:25
+ * @Version 1.0
+ **/
+public class R01 {
+}
---
+++ src/main/java/www/reader/com/effectivejava/id01chapters/Test.java
@@ -0,0 +1,13 @@
+package www.reader.com.effectivejava.id01chapters;
+/**
+ * @ClassName R01
+ * @Description TODO
+ * @Author yangkang
+ * @Date 2019/9/25 10:25
+ * @Version 1.0
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
